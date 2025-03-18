b2fc1e33cc835b563bbb84c05b86a9b5d5ea2cfe
NishantSanghani1103 <nishantsanghani09@gmail.com>
2025-03-18 11:57:22 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+$('#indrustiesSlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('#serviceSlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('#clientSlider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
