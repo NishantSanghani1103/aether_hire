e4beff9ac2a8a155ff2846e943bb5d9f76d778cb
NishantSanghani1103 <nishantsanghani09@gmail.com>
2025-03-19 21:18:03 +0530

    second commit

--- js/app.js
+++ js/app.js
@@ -1,51 +1,62 @@
 $('#indrustiesSlider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 })
 $('#serviceSlider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 })
 $('#clientSlider').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 20,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:2
+        1000: {
+            items: 2
         }
+    }
+})
+
+let arrrow = document.querySelector(".arrrow");
+arrrow.style.display = "none"
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 100) {
+        arrrow.style.display = "block";
+    } else {
+
+        arrrow.style.display = "none";
     }
 })
